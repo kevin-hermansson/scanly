194396d1b09a78a75e6bf3417bc7231805f270b5
Kevin Hermansson <hermanssonkevin@gmail.com>
2026-09-17 14:48:53 +0200

    Add health endpoint test

--- tests/Scanly.Api.Tests/UnitTest1.cs
+++ tests/Scanly.Api.Tests/UnitTest1.cs
@@ -1,10 +1,23 @@
-﻿namespace Scanly.Api.Tests;
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
 
-public class UnitTest1
+namespace Scanly.Api.Tests;
+
+public class HealthEndpointTests
+    : IClassFixture<WebApplicationFactory<Program>>
 {
-    [Fact]
-    public void Test1()
-    {
+    private readonly HttpClient _client;
 
+    public HealthEndpointTests(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Health_Returns200Ok()
+    {
+        var response = await _client.GetAsync("/health");
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
 }
